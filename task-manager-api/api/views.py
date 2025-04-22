5b631f9e31bdcc51fd0d81a88f389f9a36862946
11172410 <mariam.ayoub13@yahoo.co.uk>
2025-04-22 13:20:40 +0400

    create home route for api

---
+++ task-manager-api/api/views.py
@@ -0,0 +1,6 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view()
+def home_route(request):
+    return Response({'message': 'This is the homepage of the DTS developer challenge API.'})
