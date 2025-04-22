c36c729cba72cd8044c1d84251c774d35e0383a0
11172410 <mariam.ayoub13@yahoo.co.uk>
2025-04-22 13:23:01 +0400

    maint: add comment to home route view

--- task-manager-api/api/views.py
+++ task-manager-api/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+# Create a simple message for the index of the api
 @api_view()
 def home_route(request):
     return Response({'message': 'This is the homepage of the DTS developer challenge API.'})
