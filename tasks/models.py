a716e363bf01c872ead5ecae9627d930b4c2a306
11172410 <mariam.ayoub13@yahoo.co.uk>
2025-04-22 15:11:09 +0400

    create task model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+class Task(models.Model):
+    """
+    Task model for the API, created following the ERD diagram loacted in README.md
+    """
+    title = models.CharField(max_length=250, unique=True)
+    description = models.TextField(blank=True)
+    status = models.BooleanField(default=False)
+    due_date = models.DateField()
+    due_time = models.TimeField()
+
+    class Meta:
+        ordering = ["due_date"]
+    
+    def __str__(self):
+        return f"{self.title} | due: {self.due_date}"
