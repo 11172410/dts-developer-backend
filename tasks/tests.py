ee6f5ced7bfea391fcc7733614c2f47ba77d0bf1
11172410 <mariam.ayoub13@yahoo.co.uk>
2025-04-23 15:30:20 +0400

    test task creation

--- tasks/tests.py
+++ tasks/tests.py
@@ -16,5 +16,20 @@ class TaskListViewTests(APITestCase):
         )
 
     def test_retrieve_list_of_tasks(self):
+        """Tests if a list of all tasks can be retrieved"""
         response = self.client.get("/tasks/")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_new_task(self):
+        """Tests if new task can be created"""
+        response = self.client.post(
+            "/tasks/",
+            {
+                "title": "test task",
+                "description": "test",
+                "due_date": "2025-05-01",
+                "due_time": "12:00",
+            },
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
