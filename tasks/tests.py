0771bc09f060b83a051212796c5196dd54fe2fc5
11172410 <mariam.ayoub13@yahoo.co.uk>
2025-04-23 15:19:34 +0400

    create test for retrieving task list

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import Task
 
 # Create your tests here.
+
+
+class TaskListViewTests(APITestCase):
+    def setUp(self):
+        Task.objects.create(
+            title="test title",
+            description="test",
+            status=False,
+            due_date="2025-05-01",
+            due_time="12:00",
+        )
+
+    def test_retrieve_list_of_tasks(self):
+        response = self.client.get("/tasks/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
