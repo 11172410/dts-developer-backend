f80755bf3d0768f333c4938ff245cd06d2bf4123
11172410 <mariam.ayoub13@yahoo.co.uk>
2025-04-23 13:40:16 +0400

    create tasklist generic view

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Task
+from .serializers import TaskSerializer
 
 # Create your views here.
+
+
+class TaskList(generics.ListCreateAPIView):
+    """
+    Generic view for task list, where all tasks are retrieved as a list. Tasks can also be created with this API endpoint.
+    """
+
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
