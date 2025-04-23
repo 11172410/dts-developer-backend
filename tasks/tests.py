839a43e07f87bb2142cb1ad63dbb5a3bf4e4f6f5
11172410 <mariam.ayoub13@yahoo.co.uk>
2025-04-23 15:39:36 +0400

    test task creation in past + with missing required field

--- tasks/tests.py
+++ tasks/tests.py
@@ -49,3 +49,29 @@ class TaskListViewTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data["description"], "")
 
+    def test_create_task_with_missing_required_fields(self):
+        """Tests that a task will not be created if a required field is missing."""
+
+        response = self.client.post(
+            "/tasks/",
+            {
+                "due_date": "2025-05-01",
+                "due_time": "12:00",
+            },
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_task_set_in_the_past(self):
+        """Tests that a task will not be created if user tries to set task in the past"""
+
+        response = self.client.post(
+            "/tasks/",
+            {
+                "title": "test task",
+                "due_date": "2025-04-20",
+                "due_time": "12:00",
+            },
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
