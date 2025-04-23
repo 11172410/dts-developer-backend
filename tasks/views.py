a898576a332ed3e582f0569bb2ff07617c129cd1
11172410 <mariam.ayoub13@yahoo.co.uk>
2025-04-23 13:42:18 +0400

    create taskdetail generic view

--- tasks/views.py
+++ tasks/views.py
@@ -12,3 +12,12 @@ class TaskList(generics.ListCreateAPIView):
 
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+
+class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Generic view to allow individual tasks to be retrieved, updated and deleted by their id.
+    """
+
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
